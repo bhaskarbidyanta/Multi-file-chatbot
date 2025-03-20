81bddd5789a01c051673ae132f1f1c5e6007cb5d
bhaskarbidyanta <bhaskar.bid18@gmail.com>
2025-03-20 21:36:42 +0530

    Resolving db.py

--- db.py
+++ db.py
@@ -6,16 +6,21 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-# Encode username and password properly
-username = urllib.parse.quote_plus(os.getenv("MONGO_USERNAME"))
-password = urllib.parse.quote_plus(os.getenv("MONGO_PASSWORD"))
+# Fetch credentials
+username = os.getenv("MONGO_USERNAME")
+password = os.getenv("MONGO_PASSWORD")
 cluster = os.getenv("MONGO_CLUSTER")
 
-# Construct the MongoDB URI
+# Ensure credentials are set
+if not username or not password or not cluster:
+    raise ValueError("MONGO_USERNAME, MONGO_PASSWORD, or MONGO_CLUSTER is missing in environment variables.")
 
-MONGO_URI= f"mongodb+srv://{username}:{password}@cluster0.gaykm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-#MONGO_URI = os.getenv("MONGO_URI")
-# Connect to MongoDB
+# Encode username and password properly
+username = urllib.parse.quote_plus(username)
+password = urllib.parse.quote_plus(password)
+
+# Construct the MongoDB URI
+MONGO_URI = f"mongodb+srv://{username}:{password}@{cluster}/?retryWrites=true&w=majority"
 
 # Connect to MongoDB
 client = pymongo.MongoClient(MONGO_URI)
@@ -24,3 +29,5 @@ db = client["pdf_chatbot"]
 # Collections
 users_collection = db["users"]
 pdfs_collection = db["pdfs"]
+
+print("Connected to MongoDB successfully!")
